7428fc82ba1df071a33fb7517d4c04dfe29fa9d9
Peter Francis Robante <robantepeterfran@gmail.com>
2023-03-05 14:08:14 +0800

    cleaning spaces

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
     
      <Route path = '' element={<AutLogin/>} />
      <Route path = 'registration' element={<AutRegister/>} />
-    
     </Routes>
     </BrowserRouter>
   );
